29d53e038eaa1c99c5cfeac8bd3ecb81f9ef7a37
beenyamin <beenyaminkhan@gmail.com>
2023-11-26 19:06:50 +0600

    added jwt token for verify user

--- index.js
+++ index.js
@@ -53,7 +53,22 @@ const client = new MongoClient(uri, {
   async function run() {
     try {
 
-     
+      // jwt
+      app.post('/jwt', async (req, res) => {
+        const user = req.body
+        console.log('New Token', user)
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+          expiresIn: '10d',
+        })
+        res
+          .cookie('token', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+          })
+          .send({ success: true })
+      })
+  
       // Logout
       app.get('/logout', async (req, res) => {
         try {
