228fc89109b032061b049a8c66a42e102a21e30d
beenyamin <beenyaminkhan@gmail.com>
2023-11-30 22:47:33 +0600

    delete the specific campaign and comment the pinged line

--- index.js
+++ index.js
@@ -273,13 +273,16 @@ res.send (result)
       })
 
 
-
-
-
+      app.delete('/campaign/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) }
+        const result = await campaignCollection.deleteOne(query);
+        res.send(result);
+      })
 
     
-      await client.db('admin').command({ ping: 1 }) // TODO: comment this line before deploy
-      console.log( 'Pinged your deployment. You successfully connected to MongoDB!')
+      // await client.db('admin').command({ ping: 1 }) 
+      // console.log( 'Pinged your deployment. You successfully connected to MongoDB!')
     } finally {
       
       // await client.close();
